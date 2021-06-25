a0f9e08935993f59df9e07fbc4fb60d6c6a25322
David Armendariz <darmendariz1998@outlook.com>
2021-06-25 17:29:58 -0500

    Handled no data case

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -8,7 +8,10 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
-      <ul v-else>
+      <ul v-else-if="!isLoading && (!results || results.length === 0)">
+        <p>No stored experiences found. Start adding some survey results.</p>
+      </ul>
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
