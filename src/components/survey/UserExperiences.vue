3ddb52b44101fde7569c10139133c416297f8c0b
David Armendariz <darmendariz1998@outlook.com>
2021-06-25 17:19:07 -0500

    Fetched data from firebase

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,10 +23,36 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
-    SurveyResult,
+    SurveyResult
   },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(process.env.VUE_APP_FIREBASE_REALTIME_DATABASE)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(data => {
+          const results = [];
+          for (const id in data) {
+            const { name, rating } = data[id];
+            results.push({
+              id,
+              name,
+              rating
+            });
+          }
+          this.results = results;
+        });
+    }
+  }
 };
 </script>
 
